925b014361c5a2adb9fb917ce0e061f6003a0257
Ivan <1boomivan2@gmail.com>
2023-04-26 01:30:05 +0700

    changing the main in accordance with the update

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -1,48 +1,45 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include <libgeometry/data_x.h>
-#include <libgeometry/data_y.h>
+#include <libgeometry/calculator.h>
 #include <libgeometry/error.h>
+#include <libgeometry/lexer.h>
 #include <libgeometry/parser.h>
-#include <libgeometry/radius_data.h>
-
-#define BLUE "\e[1;36m"
-#define END "\e[0m"
-#define MAX 100
 
 int main()
 {
-    char enter[MAX], shape[MAX];
-    int num = 0;
-
-    printf("Enter the name and coordinates of the shape (or press Enter for "
-           "exit):\n");
-    fgets(enter, MAX, stdin);
-
-    for (int i = 0; i < strlen(enter); i++) {
-        if (enter[i] == '(' || enter[i] == ' ') {
-            to_lower(shape, num);
-            if (strcmp(shape, "circle") == 0) {
-                struct point Center = find_center(enter, &num);
-                struct circle CIRCLE = find_out_circle(&Center, enter, &num);
-                empty(enter, &num);
-                output_circle_message(&CIRCLE);
-                break;
-            } else {
-                show_bugs(BUG_NAME, 0, NULL);
+    char ch;
+    struct circle circles[NUM_OF_LAPS];
+    int count = 0;
+    while ((count < NUM_OF_LAPS)
+           && printf("Enter a geometric figure (or press Q for exit):\n")
+           && ((ch = getc(stdin)) != 'q')) {
+        char enter[MAX], shape[MAX] = {};
+        int num = 0;
+        fgets(enter, MAX, stdin);
+        first_character(enter, ch);
+        for (int i = 0; i < strlen(enter); i++) {
+            if (enter[i] == '(') {
+                to_lower(shape, num);
+                if (strcmp(shape, "circle") == 0) {
+                    circles[count] = find_center(enter, &num);
+                    shape_characteristics(&circles[count]);
+                    output_circle_message(&circles[count++], shape);
+                    break;
+                } else {
+                    show_bugs(BUG_NAME, 0, NULL);
+                    exit(1);
+                }
+            } else if (enter[i] == ')' || enter[i] == ' ') {
+                show_bugs(BUG_STAPLES, num, &enter[i]);
                 exit(1);
             }
-        } else if (enter[i] == ')') {
-            show_bugs(BUG_STAPLES, num, &enter[i]);
-            exit(1);
+            shape[num] = enter[i];
+            num++;
         }
-
-        shape[num] = enter[i];
-        num++;
     }
+    show_intersection(circles, count);
 
     return 0;
 }
